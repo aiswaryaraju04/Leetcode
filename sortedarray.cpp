18e51153b76cf76b03ffe0d869b98a7c7cbc4ca5
Aiswarya Raju <aiswaryaraju2001@gmail.com>
2022-06-04 10:26:08 +0530

    new file

---
+++ sortedarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        int temp=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i]= nums[i]*nums[i];
+        }
+       
+        
+        sort(nums.begin(),nums.end());
+        
+    
+    return nums;
+    }
+};
